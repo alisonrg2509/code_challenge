3fa42712828afdbaf4c56800c98279b4935e8052
alisonrg2509 <alisondegasperifirmes@gmail.com>
2024-12-16 11:48:08 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Exercicio2 {
+  
+    private static final double raio = 7.5;
+
+    public static void main(String[] args) {
+        double perimetro = 2 * Math.PI * raio;
+
+        double area = Math.PI * raio * raio;
+
+        System.out.println("Perimetro = " + perimetro);
+        System.out.println("Area = " + area);
+    }
+}
